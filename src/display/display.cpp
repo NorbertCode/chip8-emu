15f6967b4dfdab03a80426c7be8c5ed51606518a
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-04-12 00:40:05 +0200

    display class

---
+++ src/display/display.cpp
@@ -0,0 +1,48 @@
+#include "display.hpp"
+
+Display::Display(const std::uint8_t width, const std::uint8_t height)
+{
+    display.resize(width, std::vector<bool>(height));
+}
+
+const std::vector<std::vector<bool>>& Display::getDisplay() const
+{
+    return display;
+}
+
+bool Display::xorPixel(const std::uint8_t x, const std::uint8_t y, const bool value)
+{
+    bool collision = display[x][y] && !value;
+
+    display[x][y] = display[x][y] != value;
+
+    return collision;
+}
+
+bool Display::xorSprite(const std::uint8_t x, const std::uint8_t y, const std::vector<std::uint8_t> sprite)
+{
+    bool collision = false;
+
+    for (size_t sprite_row_index = 0; sprite_row_index < sprite.size(); ++sprite_row_index)
+    {
+        std::uint8_t sprite_row = sprite[sprite_row_index];
+        std::uint8_t display_row_index = y + sprite_row_index;
+
+        for (size_t sprite_column_index = 0; sprite_column_index < 8; ++sprite_column_index)
+        {
+            bool sprite_pixel = ((sprite_row >> (7 - sprite_column_index)) & 0x1) > 0;
+            collision = xorPixel(x + sprite_column_index, display_row_index, sprite_pixel) || collision;
+        }
+    }
+
+    return collision;
+}
+
+void Display::clear()
+{
+    for (size_t i = 0; i < display.size(); ++i)
+    {
+        for (size_t j = 0; j < display[i].size(); ++j)
+            display[i][j] = false;
+    }
+}
---
+++ src/display/display.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+class Display
+{
+public:
+    // Coordinates are taken from registers which are 8-bit, hence std::uint8_t
+    Display(const std::uint8_t width, const std::uint8_t height);
+
+    const std::vector<std::vector<bool>>& getDisplay() const;
+
+    bool xorPixel(const std::uint8_t x, const std::uint8_t y, const bool value); // Returns collision (if pixel was erased)
+    bool xorSprite(const std::uint8_t x, const std::uint8_t y, const std::vector<std::uint8_t> sprite); // Returns collision (if pixel was erased)
+    void clear();
+
+private:
+    std::vector<std::vector<bool>> display;
+
+};
