4752eacadf8fde8a9591964eee0587be0de995bd
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-04-22 02:42:13 +0200

    implemented ldB opcode

--- src/processor/opcodes.cpp
+++ src/processor/opcodes.cpp
@@ -171,7 +171,29 @@ void Processor::ldF(const std::uint8_t x)
 
 void Processor::ldB(const std::uint8_t x)
 {
-    // TODO: Double Dabble Alg
+    // Double Dabble Alg
+    const int digit_bits = 12; // 8-bit value needs 12 bits for BCD representation
+    const int digits = digit_bits / 4;
+
+    std::uint32_t scratch_space = x;
+
+    for (int i = 0; i < 8; ++i) // For every bit in x
+    {
+        for (unsigned int digit_bit = 8; digit_bit < 8 + digit_bits; digit_bit+=4) // Start at BCD representation offset
+        {
+            std::uint8_t digit = static_cast<std::uint8_t>(scratch_space >> digit_bit) & 0xF; 
+
+            scratch_space += (digit >= 5) * (3U << digit_bit); // Add 3 to the correct BCD digit if its >= 5
+        }
+
+        scratch_space <<= 1;
+    }
+
+    for (int i = 0; i < digits; ++i)
+    {
+        std::uint8_t digit = static_cast<std::uint8_t>(scratch_space >> (8 + 4 * (digits - i - 1))) & 0xF;
+        memory.write(registerI + i, digit);
+    }
 }
 
 void Processor::ldIReg(const std::uint8_t x)
