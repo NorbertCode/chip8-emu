8efb1975d88d9ea5009f2a319512ddb2d689e45a
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-08-05 20:11:31 +0200

    fixed weird opcodes include

--- src/chip8_core/processor/processor.cpp
+++ src/chip8_core/processor/processor.cpp
@@ -1,5 +1,4 @@
 #include "processor.hpp"
-#include "opcodes.cpp"
 
 Processor::Processor(Memory& memory, Storage& storage, Display& display, Keyboard& keyboard, const Quirks& quirks, std::uint16_t startProgramCounter)
     : memory(memory), storage(storage), display(display), keyboard(keyboard), quirks(quirks), random(std::random_device{}()), uniformDistribution(0, 255)
