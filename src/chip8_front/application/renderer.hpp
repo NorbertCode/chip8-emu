fbd058db900c5dfe2422a9147df333f7518b91c9
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-08-05 20:26:43 +0200

    added missing constructors

--- src/chip8_front/application/renderer.hpp
+++ src/chip8_front/application/renderer.hpp
@@ -9,6 +9,11 @@ public:
     Renderer(int width, int height, std::uint32_t foregroundColor, std::uint32_t backgroundColor);
     ~Renderer();
 
+    Renderer(const Renderer&) = delete;
+    Renderer& operator=(const Renderer&) = delete;
+    Renderer(Renderer&&) = delete;
+    Renderer& operator=(Renderer&&) = delete;
+
     void render(const std::vector<std::uint8_t>& display);
 
 private:
