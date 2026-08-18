2e68031f719780443dba5c45a959031c515a0daa
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-08-18 20:57:49 +0200

    fixed main throwing exceptions

--- src/chip8/platform/desktop/main.cpp
+++ src/chip8/platform/desktop/main.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include "desktopLoader.hpp"
 #include "application/application.hpp"
@@ -6,24 +7,32 @@ using namespace chip8;
 
 int main(int argc, char* argv[])
 {
-    front::DesktopLoader loader("CHIP8", argc, argv);
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    try
     {
-        std::cerr << "Error initializing SDL: " << SDL_GetError() << '\n';
+        front::DesktopLoader loader("CHIP8", argc, argv);
+
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        {
+            std::cerr << "Error initializing SDL: " << SDL_GetError() << '\n';
+            return 1;
+        }
+
+        auto onStorageWriteCallback = [&loader](std::span<const std::uint8_t, 16> data) {
+            loader.writeStorage(data);
+        };
+
+        core::Chip8 chip8(loader.getQuirks(), loader.getMemoryConfig(), loader.getDisplayConfig(), onStorageWriteCallback);
+        chip8.getStorage().setData(loader.readStorage());
+        chip8.loadRom(loader.getRom());
+
+        front::Application app(chip8, loader.getApplicationConfig());
+        app.run();
+
+        SDL_Quit();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
         return 1;
     }
-
-    auto onStorageWriteCallback = [&loader](std::span<const std::uint8_t, 16> data) {
-        loader.writeStorage(data);
-    };
-
-    core::Chip8 chip8(loader.getQuirks(), loader.getMemoryConfig(), loader.getDisplayConfig(), onStorageWriteCallback);
-    chip8.getStorage().setData(loader.readStorage());
-    chip8.loadRom(loader.getRom());
-
-    front::Application app(chip8, loader.getApplicationConfig());
-    app.run();
-
-    SDL_Quit();
 }
--- src/disassembler/platform/desktop/main.cpp
+++ src/disassembler/platform/desktop/main.cpp
@@ -7,29 +7,37 @@ using namespace disassembler;
 
 int main(int argc, char* argv[])
 {
-    front::DesktopLoader loader("CHIP8 DISASM", argc, argv);
+    try
+    {
+        front::DesktopLoader loader("CHIP8 DISASM", argc, argv);
 
-    core::Disassembler disasm(loader.getDisassemblerConfig());
-    std::vector<std::string> output = disasm.disassemble(loader.getInput());
-    
-    if (loader.shouldPrettify())
-    {
-        std::cout << std::format("{:<8}{:<8}{:<25}\n", "LINE", "HEX", "INSTRUCTION");
-        std::cout << std::format("{:-<36}\n", "");
-    }
-    
-    for (size_t i = 0; i < output.size(); ++i)
-    {
+        core::Disassembler disasm(loader.getDisassemblerConfig());
+        std::vector<std::string> output = disasm.disassemble(loader.getInput());
+        
         if (loader.shouldPrettify())
         {
-            std::string line = std::format("0x{:04X}", disasm.getProgramStart() + (i * 2));
-            std::string hex = std::format("0x{:04X}", loader.getInput()[i]);
-
-            std::cout << std::format("{:<8}{:<8}{:<25}\n", line, hex, output[i]);
+            std::cout << std::format("{:<8}{:<8}{:<25}\n", "LINE", "HEX", "INSTRUCTION");
+            std::cout << std::format("{:-<36}\n", "");
         }
-        else
-            std::cout << output[i] << '\n';
-    }
+        
+        for (size_t i = 0; i < output.size(); ++i)
+        {
+            if (loader.shouldPrettify())
+            {
+                std::string line = std::format("0x{:04X}", disasm.getProgramStart() + (i * 2));
+                std::string hex = std::format("0x{:04X}", loader.getInput()[i]);
 
-    return 0;
+                std::cout << std::format("{:<8}{:<8}{:<25}\n", line, hex, output[i]);
+            }
+            else
+                std::cout << output[i] << '\n';
+        }
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 }
