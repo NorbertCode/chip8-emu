ca29455f9eadc6397fd9133e62ace615d5ca5edf
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-08-29 15:05:34 +0200

    added builder for the debugger

---
+++ src/chip8/front/debugger/debuggerBuilder.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "debugger/debugWidget.hpp"
+#include "debugger/debugger.hpp"
+#include <SDL2/SDL.h>
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace chip8::front
+{
+    class DebuggerBuilder
+    {
+    public:
+        DebuggerBuilder(SDL_Window& window, SDL_Renderer& renderer);
+
+        DebuggerBuilder& addWidget(std::unique_ptr<DebugWidget> widget);
+        Debugger build();
+
+    private:
+        std::vector<std::unique_ptr<DebugWidget>> widgets;
+
+        std::reference_wrapper<SDL_Window> window;
+        std::reference_wrapper<SDL_Renderer> renderer;
+    };
+}
