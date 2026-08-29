92bb0e4725cb62a9b6f52ddc944db614a4595134
Norbert <54719382+NorbertCode@users.noreply.github.com>
2026-08-29 22:02:21 +0200

    made viewport center itself

--- src/chip8/front/debugger/widgets/viewportWidget.cpp
+++ src/chip8/front/debugger/widgets/viewportWidget.cpp
@@ -11,9 +11,31 @@ namespace chip8::front
     {
         ImGui::Begin("Viewport");
 
+        ImVec2 viewportSize = ImGui::GetContentRegionAvail();
+        ImVec2 displaySize;
+
+        float aspectRatio = viewportSize.x / viewportSize.y;
+        float targetAspectRatio = static_cast<float>(renderer.get().getWidth()) / static_cast<float>(renderer.get().getHeight());
+
+        if (aspectRatio > targetAspectRatio)
+        {
+            displaySize.x = targetAspectRatio * viewportSize.y;
+            displaySize.y = viewportSize.y;
+        }
+        else
+        {
+            displaySize.x = viewportSize.x;
+            displaySize.y = viewportSize.x / targetAspectRatio;
+        }
+
+        float offsetX = (viewportSize.x - displaySize.x) * 0.5f;
+        float offsetY = (viewportSize.y - displaySize.y) * 0.5f;
+
+        ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + offsetX, ImGui::GetCursorPosY() + offsetY));
+        
         ImGui::Image(
             (ImTextureID)reinterpret_cast<intptr_t>(&renderer.get().getDisplayTexture()),
-            ImGui::GetContentRegionAvail()
+            displaySize
         );
 
         ImGui::End();
